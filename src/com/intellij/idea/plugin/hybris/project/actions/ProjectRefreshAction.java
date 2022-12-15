7ecdada1ea31b67330e864315920097c5fd4587a
Mykhailo Lytvyn <>
2022-12-15 16:50:12 +0100

    Decrease deprecated API usage - updated gradle version to 1.10.1 - fixed few deprecated API usages - added filter capability for deprecated beans

--- src/com/intellij/idea/plugin/hybris/project/actions/ProjectRefreshAction.java
+++ src/com/intellij/idea/plugin/hybris/project/actions/ProjectRefreshAction.java
@@ -38,8 +38,8 @@ import com.intellij.lang.ant.config.AntConfigurationBase;
 import com.intellij.openapi.actionSystem.ActionPlaces;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.DataContext;
-import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
@@ -52,9 +52,9 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.roots.CompilerProjectExtension;
 import com.intellij.openapi.roots.ProjectRootManager;
-import com.intellij.openapi.roots.impl.libraries.ProjectLibraryTable;
 import com.intellij.openapi.roots.libraries.Library;
 import com.intellij.openapi.roots.libraries.LibraryTable;
+import com.intellij.openapi.roots.libraries.LibraryTablesRegistrar;
 import com.intellij.openapi.roots.ui.configuration.ModulesProvider;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.projectImport.ProjectImportProvider;
@@ -67,8 +67,11 @@ import org.jetbrains.annotations.NotNull;
 public class ProjectRefreshAction extends AnAction {
 
     public static void triggerAction() {
-        final DataContext dataContext = DataManager.getInstance().getDataContextFromFocus().getResult();
-        triggerAction(dataContext);
+        final DataManager dataManager = DataManager.getInstance();
+        if (dataManager != null) {
+            dataManager.getDataContextFromFocusAsync()
+                       .onSuccess(ProjectRefreshAction::triggerAction);
+        }
     }
 
     public static void triggerAction(final DataContext dataContext) {
@@ -85,7 +88,7 @@ public class ProjectRefreshAction extends AnAction {
     }
 
     @Override
-    public void actionPerformed(AnActionEvent anActionEvent) {
+    public void actionPerformed(final AnActionEvent anActionEvent) {
         final Project project = getEventProject(anActionEvent);
 
         if (project == null) {
@@ -116,7 +119,7 @@ public class ProjectRefreshAction extends AnAction {
         for (Module module : moduleModel.getModules()) {
             moduleModel.disposeModule(module);
         }
-        final LibraryTable.ModifiableModel libraryModel = ProjectLibraryTable.getInstance(project).getModifiableModel();
+        final LibraryTable.ModifiableModel libraryModel = LibraryTablesRegistrar.getInstance().getLibraryTable(project).getModifiableModel();
 
         for (Library library : libraryModel.getLibraries()) {
             libraryModel.removeLibrary(library);
@@ -138,9 +141,9 @@ public class ProjectRefreshAction extends AnAction {
     }
 
     @Override
-    public void update(AnActionEvent event) {
-        final Project project = event.getData(PlatformDataKeys.PROJECT);
-        final Presentation presentation = event.getPresentation();
+    public void update(final AnActionEvent e) {
+        final Project project = e.getData(CommonDataKeys.PROJECT);
+        final Presentation presentation = e.getPresentation();
         if (project == null) {
             presentation.setVisible(false);
             return;
