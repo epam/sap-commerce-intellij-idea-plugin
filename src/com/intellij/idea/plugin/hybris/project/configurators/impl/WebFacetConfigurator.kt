a1db9994c1d55966a5fb59b63ee1d584a6ba6b44
Mykhailo Lytvyn <>
2023-06-03 13:19:17 +0200

    -

---
+++ src/com/intellij/idea/plugin/hybris/project/configurators/impl/WebFacetConfigurator.kt
@@ -0,0 +1,67 @@
+/*
+ * This file is part of "hybris integration" plugin for Intellij IDEA.
+ * Copyright (C) 2014-2016 Alexander Bartash <AlexanderBartash@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.intellij.idea.plugin.hybris.project.configurators.impl
+
+import com.intellij.facet.FacetManager
+import com.intellij.facet.FacetTypeRegistry
+import com.intellij.facet.ModifiableFacetModel
+import com.intellij.idea.plugin.hybris.common.HybrisConstants
+import com.intellij.idea.plugin.hybris.project.configurators.FacetConfigurator
+import com.intellij.idea.plugin.hybris.project.descriptors.HybrisProjectDescriptor
+import com.intellij.idea.plugin.hybris.project.descriptors.ModuleDescriptor
+import com.intellij.idea.plugin.hybris.project.descriptors.YModuleDescriptorUtil
+import com.intellij.javaee.DeploymentDescriptorsConstants
+import com.intellij.javaee.web.facet.WebFacet
+import com.intellij.openapi.application.WriteAction
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.module.ModuleType
+import com.intellij.openapi.roots.ModifiableRootModel
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.vfs.VfsUtil
+import java.io.File
+
+class WebFacetConfigurator : FacetConfigurator {
+
+    override fun configure(
+        hybrisProjectDescriptor: HybrisProjectDescriptor, modifiableFacetModel: ModifiableFacetModel,
+        moduleDescriptor: ModuleDescriptor,
+        javaModule: Module,
+        modifiableRootModel: ModifiableRootModel
+    ) {
+        val webRoot = YModuleDescriptorUtil.getWebRoot(moduleDescriptor) ?: return
+        val webFacet = modifiableFacetModel.getFacetByType(WebFacet.ID)
+            ?.also {
+                it.removeAllWebRoots()
+                it.descriptorsContainer.configuration.removeConfigFiles(DeploymentDescriptorsConstants.WEB_XML_META_DATA)
+            }
+            ?: FacetTypeRegistry.getInstance().findFacetType(WebFacet.ID)
+                .takeIf { it.isSuitableModuleType(ModuleType.get(javaModule)) }
+                ?.let { FacetManager.getInstance(javaModule).createFacet(it, it.defaultFacetName, null) }
+                ?.also { modifiableFacetModel.addFacet(it) }
+            ?: return
+
+        WriteAction.runAndWait<RuntimeException> {
+            webFacet.setWebSourceRoots(modifiableRootModel.getSourceRootUrls(false))
+            webFacet.addWebRootNoFire(VfsUtil.pathToUrl(FileUtil.toSystemIndependentName(webRoot.absolutePath)), "/")
+
+            VfsUtil.findFileByIoFile(File(moduleDescriptor.rootDirectory, HybrisConstants.WEB_XML_DIRECTORY_RELATIVE_PATH), true)
+                ?.let { webFacet.descriptorsContainer.configuration.addConfigFile(DeploymentDescriptorsConstants.WEB_XML_META_DATA, it.url) }
+        }
+    }
+
+}
