360fa72170f64aa230bb41bd19f397cae1faac1b
Stefan Kruk <stefan.kruk@gmx.net>
2024-02-25 16:42:52 +0100

    Update License Header and CHANGELOG.md

--- src/com/intellij/idea/plugin/hybris/project/configurators/impl/DefaultJavadocSettingsConfigurator.kt
+++ src/com/intellij/idea/plugin/hybris/project/configurators/impl/DefaultJavadocSettingsConfigurator.kt
@@ -1,5 +1,6 @@
 /*
- * This file is part of "SAP Commerce Developers Toolset" plugin for IntelliJ IDEA.
+ * This file is part of "SAP Commerce Developers Toolset" plugin for Intellij IDEA.
+ * Copyright (C) 2014-2016 Alexander Bartash <AlexanderBartash@gmail.com>
  * Copyright (C) 2019-2024 EPAM Systems <hybrisideaplugin@epam.com> and contributors
  *
  * This program is free software: you can redistribute it and/or modify
@@ -15,7 +16,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 package com.intellij.idea.plugin.hybris.project.configurators.impl
 
 import com.intellij.idea.plugin.hybris.project.configurators.JavadocSettingsConfigurator
@@ -33,7 +33,7 @@ class DefaultJavadocSettingsConfigurator : JavadocSettingsConfigurator {
 
         moduleDescriptor.rootProjectDescriptor.getJavadocUrl()
             ?.takeUnless { moduleDescriptor is YCustomRegularModuleDescriptor }
-            ?.takeUnless { moduleDescriptor is CCv2ModuleDescriptor }
+             ?.takeUnless { moduleDescriptor is CCv2ModuleDescriptor }
             ?.takeUnless { moduleDescriptor is ConfigModuleDescriptor }
             ?.let { javadocRefList.add(it) }
 
