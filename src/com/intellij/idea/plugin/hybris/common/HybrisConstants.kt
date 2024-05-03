a5f9b441aa6e897390b6bd244c10198f1497e7c6
Mykhailo Lytvyn <lytvyn.michael@gmail.com>
2024-05-03 13:12:12 +0200

    Removed `SAP CCM` based integration

--- src/com/intellij/idea/plugin/hybris/common/HybrisConstants.kt
+++ src/com/intellij/idea/plugin/hybris/common/HybrisConstants.kt
@@ -30,7 +30,6 @@ import com.intellij.idea.plugin.hybris.project.descriptors.ModuleDescriptor
 import com.intellij.openapi.util.Key
 import com.intellij.openapi.util.io.FileUtilRt
 import com.intellij.psi.tree.IFileElementType
-import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
 object HybrisConstants {
@@ -416,7 +415,6 @@ object HybrisConstants {
     val OCC_DEFAULT_LEVEL_MAPPINGS = setOf("BASIC", "DEFAULT", "FULL")
 
     val CCV2_DATE_TIME_FORMATTER_LOCAL: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd | HH:mm:ss")
-    val ZONE_GMT = ZoneId.of("GMT")
 
     @JvmField
     val IMPEX_MODIFIER_BOOLEAN_VALUES = setOf("true", "false")
