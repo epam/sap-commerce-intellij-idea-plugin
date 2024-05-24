82e1bc941e69e13a56c54a1912491dc2835a6c77
Mykhailo Lytvyn <lytvyn.michael@gmail.com>
2024-05-24 19:02:26 +0200

    Added reference resolution for index `include`:`attribute`

--- src/com/intellij/idea/plugin/hybris/system/type/psi/contributor/TSReferenceContributor.kt
+++ src/com/intellij/idea/plugin/hybris/system/type/psi/contributor/TSReferenceContributor.kt
@@ -1,6 +1,6 @@
 /*
- * This file is part of "SAP Commerce Developers Toolset" plugin for Intellij IDEA.
- * Copyright (C) 2019-2023 EPAM Systems <hybrisideaplugin@epam.com> and contributors
+ * This file is part of "SAP Commerce Developers Toolset" plugin for IntelliJ IDEA.
+ * Copyright (C) 2019-2024 EPAM Systems <hybrisideaplugin@epam.com> and contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -26,6 +26,10 @@ import com.intellij.psi.PsiReferenceRegistrar
 class TSReferenceContributor : PsiReferenceContributor() {
 
     override fun registerReferenceProviders(registrar: PsiReferenceRegistrar) {
+        registrar.registerReferenceProvider(
+            TSPatterns.INDEX_KEY_ATTRIBUTE,
+            TSItemAttributeReferenceProvider.instance
+        )
         registrar.registerReferenceProvider(
             TSPatterns.INDEX_KEY_ATTRIBUTE,
             TSItemAttributeReferenceProvider.instance
