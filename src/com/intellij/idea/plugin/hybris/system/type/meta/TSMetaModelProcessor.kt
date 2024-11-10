8a83cc052b7cfb53d46bc1f4b0d389a7429f81e6
Mykhailo Lytvyn <lytvyn.michael@gmail.com>
2024-11-10 21:54:31 +0100

    Migrated Type System Meta Model creation to coroutines

--- src/com/intellij/idea/plugin/hybris/system/type/meta/TSMetaModelProcessor.kt
+++ src/com/intellij/idea/plugin/hybris/system/type/meta/TSMetaModelProcessor.kt
@@ -1,6 +1,6 @@
 /*
- * This file is part of "SAP Commerce Developers Toolset" plugin for Intellij IDEA.
- * Copyright (C) 2019 EPAM Systems <hybrisideaplugin@epam.com>
+ * This file is part of "SAP Commerce Developers Toolset" plugin for IntelliJ IDEA.
+ * Copyright (C) 2019-2024 EPAM Systems <hybrisideaplugin@epam.com> and contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -17,13 +17,12 @@
  */
 package com.intellij.idea.plugin.hybris.system.type.meta
 
-import com.intellij.idea.plugin.hybris.common.utils.HybrisI18NBundleUtils
 import com.intellij.idea.plugin.hybris.system.type.meta.impl.TSMetaModelBuilder
 import com.intellij.idea.plugin.hybris.system.type.model.Items
 import com.intellij.idea.plugin.hybris.system.type.model.TypeGroup
 import com.intellij.idea.plugin.hybris.system.type.util.TSUtils
+import com.intellij.openapi.application.readActionBlocking
 import com.intellij.openapi.components.Service
-import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
 import com.intellij.psi.xml.XmlFile
@@ -31,23 +30,23 @@ import com.intellij.util.xml.DomManager
 import java.util.stream.Collectors
 
 @Service(Service.Level.PROJECT)
-class TSMetaModelProcessor(private val myProject: Project) {
+class TSMetaModelProcessor(myProject: Project) {
 
     private val myDomManager: DomManager = DomManager.getDomManager(myProject)
 
-    fun process(psiFile: PsiFile): TSMetaModel? {
-        psiFile.virtualFile ?: return null
-        val module = TSUtils.getModuleForFile(psiFile) ?: return null
+    suspend fun process(psiFile: PsiFile): TSMetaModel? = readActionBlocking {
+
+
+        psiFile.virtualFile ?: return@readActionBlocking null
+        val module = TSUtils.getModuleForFile(psiFile) ?: return@readActionBlocking null
         val custom = TSUtils.isCustomExtensionFile(psiFile)
         val rootWrapper = myDomManager.getFileElement(psiFile as XmlFile, Items::class.java)
 
-        rootWrapper ?: return null
+        rootWrapper ?: return@readActionBlocking null
 
         val items = rootWrapper.rootElement
 
-        ProgressManager.getInstance().progressIndicator.text2 = HybrisI18NBundleUtils.message("hybris.ts.access.progress.subTitle.processing", psiFile.name)
-
-        return TSMetaModelBuilder(module, psiFile, custom)
+        TSMetaModelBuilder(module, psiFile, custom)
             .withItemTypes(items.itemTypes.itemTypes)
             .withItemTypes(items.itemTypes.typeGroups.stream()
                 .flatMap { tg: TypeGroup -> tg.itemTypes.stream() }
