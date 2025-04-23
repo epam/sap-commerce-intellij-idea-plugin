bbd643b34ad81a73e5b5e1e98e2163c44590752c
Stefan Kruk <stefan.kruk@gmx.net>
2025-04-23 12:48:37 +0200

    #1293 load VM Properties from wrapper configs instead of Properties File

--- src/com/intellij/idea/plugin/hybris/common/HybrisConstants.kt
+++ src/com/intellij/idea/plugin/hybris/common/HybrisConstants.kt
@@ -168,6 +168,11 @@ object HybrisConstants {
     const val PLATFORM_LIBRARY_GROUP = "Platform Bootstrap"
     const val PLATFORM_DATABASE_DRIVER_LIBRARY = "Database Drivers"
 
+	const val TOMCAT_WRAPPER_CONFIG_DIR = "conf"
+	const val WRAPPER_FILE_PREFIX = "wrapper"
+	const val WRAPPER_FILE_SUFFIX = ".conf"
+	const val DEFAULT_WRAPPER_FILENAME = "wrapper.conf"
+
     const val NODE_MODULES_DIRECTORY = "node_modules"
     const val JS_TARGET_DIRECTORY = "jsTarget"
     const val BOWER_COMPONENTS_DIRECTORY = "bower_components"
--- src/com/intellij/idea/plugin/hybris/properties/PropertyService.kt
+++ src/com/intellij/idea/plugin/hybris/properties/PropertyService.kt
@@ -19,6 +19,10 @@
 package com.intellij.idea.plugin.hybris.properties
 
 import com.intellij.idea.plugin.hybris.common.HybrisConstants
+import com.intellij.idea.plugin.hybris.common.HybrisConstants.DEFAULT_WRAPPER_FILENAME
+import com.intellij.idea.plugin.hybris.common.HybrisConstants.PLATFORM_TOMCAT_DIRECTORY
+import com.intellij.idea.plugin.hybris.common.HybrisConstants.TOMCAT_WRAPPER_CONFIG_DIR
+import com.intellij.idea.plugin.hybris.common.HybrisConstants.WRAPPER_FILE_SUFFIX
 import com.intellij.idea.plugin.hybris.common.yExtensionName
 import com.intellij.idea.plugin.hybris.project.utils.HybrisRootUtil
 import com.intellij.lang.properties.IProperty
@@ -30,6 +34,7 @@ import com.intellij.openapi.components.Service
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.util.ModificationTracker
 import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VirtualFile
@@ -42,6 +47,7 @@ import com.intellij.psi.util.CachedValuesManager
 import com.intellij.util.asSafely
 import com.intellij.util.concurrency.AppExecutorUtil
 import java.io.File
+import java.io.InputStreamReader
 import java.util.*
 import java.util.regex.Pattern
 
@@ -179,6 +185,46 @@ class PropertyService(val project: Project) {
     fun getPlatformHome(): String? {
         return HybrisRootUtil.findPlatformRootDirectory(project)?.path
     }
+
+	fun getTomcatWrapperProperties(executionId: String? = null): Properties {
+		val platformModule = obtainPlatformModule()
+			?: throw IllegalStateException("Platform module not found")
+
+		val tomcatDir = findTomcatDirectory(platformModule)
+
+		val configFileName = when (executionId) {
+			null -> DEFAULT_WRAPPER_FILENAME
+			else -> "wrapper-$executionId$WRAPPER_FILE_SUFFIX"
+		}
+
+		val confFile = tomcatDir.findFileByRelativePath("$TOMCAT_WRAPPER_CONFIG_DIR/$configFileName")
+		return loadProperties(confFile)
+	}
+
+	private fun findTomcatDirectory(platformModule: Module): VirtualFile {
+		return ModuleRootManager.getInstance(platformModule)
+				.contentRoots
+				.asSequence()
+				.mapNotNull { it.findFileByRelativePath(PLATFORM_TOMCAT_DIRECTORY) }
+				.firstOrNull()
+			?: throw IllegalStateException("Tomcat directory not found")
+	}
+
+	private fun loadProperties(confFile: VirtualFile?): Properties {
+		val properties = Properties()
+
+		try {
+			confFile?.inputStream?.let { inputStream ->
+				InputStreamReader(inputStream).use { reader ->
+					properties.load(reader)
+				}
+			}
+		} catch (e: Exception) {
+			e.printStackTrace()
+		}
+
+		return properties
+	}
 
     private fun replacePlaceholder(result: LinkedHashMap<String, String>, key: String, visitedProperties: MutableSet<String>) {
 
