7e57ddf27544aef75425f6826112c40a7797ae3e
Mykhailo Lytvyn <lytvyn.michael@gmail.com>
2025-05-01 17:53:48 +0200

    - make Eclipse modules preselected

--- src/com/intellij/idea/plugin/hybris/project/descriptors/impl/EclipseModuleDescriptor.kt
+++ src/com/intellij/idea/plugin/hybris/project/descriptors/impl/EclipseModuleDescriptor.kt
@@ -1,6 +1,6 @@
 /*
- * This file is part of "SAP Commerce Developers Toolset" plugin for Intellij IDEA.
- * Copyright (C) 2019 EPAM Systems <hybrisideaplugin@epam.com>
+ * This file is part of "SAP Commerce Developers Toolset" plugin for IntelliJ IDEA.
+ * Copyright (C) 2019-2025 EPAM Systems <hybrisideaplugin@epam.com> and contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -26,4 +26,7 @@ class EclipseModuleDescriptor(
     rootProjectDescriptor: HybrisProjectDescriptor,
     name: String,
     override val descriptorType: ModuleDescriptorType = ModuleDescriptorType.ECLIPSE
-) : RootModuleDescriptor(moduleRootDirectory, rootProjectDescriptor, name)
+) : RootModuleDescriptor(moduleRootDirectory, rootProjectDescriptor, name) {
+
+    override fun isPreselected() = true
+}
