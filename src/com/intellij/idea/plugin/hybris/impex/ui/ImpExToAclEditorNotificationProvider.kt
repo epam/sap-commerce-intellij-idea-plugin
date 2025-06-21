64f8d45607633150383138cbdca54cc19fdcc0ef
Mykhailo Lytvyn <lytvyn.michael@gmail.com>
2025-06-21 22:25:10 +0200

    - added copyright support
    Signed-off-by: Mykhailo Lytvyn <lytvyn.michael@gmail.com>

---
+++ src/com/intellij/idea/plugin/hybris/impex/ui/ImpExToAclEditorNotificationProvider.kt
@@ -0,0 +1,35 @@
+/*
+ * This file is part of "SAP Commerce Developers Toolset" plugin for IntelliJ IDEA.
+ * Copyright (C) 2019-2025 EPAM Systems <hybrisideaplugin@epam.com> and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.intellij.idea.plugin.hybris.impex.ui
+
+import com.intellij.openapi.fileEditor.FileEditor
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.ui.EditorNotificationProvider
+import java.util.function.Function
+import javax.swing.JComponent
+
+class ImpExToAclEditorNotificationProvider : EditorNotificationProvider {
+    override fun collectNotificationData(
+        project: Project,
+        file: VirtualFile
+    ): Function<in FileEditor, out JComponent?>? {
+        return null
+//        TODO("Not yet implemented")
+    }
+}
