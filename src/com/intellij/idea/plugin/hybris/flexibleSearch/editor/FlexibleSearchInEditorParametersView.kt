28587516058c749871c82e8e60b83064eb2d5509
Mykhailo Lytvyn <lytvyn.michael@gmail.com>
2025-07-13 21:16:13 +0200

    - sr
    Signed-off-by: Mykhailo Lytvyn <lytvyn.michael@gmail.com>

--- src/com/intellij/idea/plugin/hybris/flexibleSearch/editor/FlexibleSearchInEditorParametersView.kt
+++ src/com/intellij/idea/plugin/hybris/flexibleSearch/editor/FlexibleSearchInEditorParametersView.kt
@@ -152,9 +152,12 @@ object FlexibleSearchInEditorParametersView {
                         "byte", "java.lang.Byte" -> numberTextField(parameter, fileEditor, "-128", "127", "byte") { it.toByteOrNull() == null }
                         "short", "java.lang.Short" -> numberTextField(parameter, fileEditor, "-32,768", "32,767", "short") { it.toShortOrNull() == null }
                         "int", "java.lang.Integer" -> numberTextField(parameter, fileEditor, "-2,147,483,648", "2,147,483,647", "int") { it.toIntOrNull() == null }
-                        "long", "java.lang.Long" -> numberTextField(parameter, fileEditor, "-9,223,372,036,854,775,808", "9,223,372,036,854,775,807", "long") { it.toLongOrNull() == null }
                         "float", "java.lang.Float" -> numberTextField(parameter, fileEditor, "1.4E-45F", "3.4E+38F", "float") { it.toFloatOrNull() == null }
                         "double", "java.lang.Double" -> numberTextField(parameter, fileEditor, "4.9E-324", "1.8E+308", "double") { it.toDoubleOrNull() == null }
+                        "long", "java.lang.Long" -> numberTextField(
+                            parameter, fileEditor,
+                            "-9,223,372,036,854,775,808", "9,223,372,036,854,775,807", "long"
+                        ) { it.toLongOrNull() == null }
 
                         "boolean",
                         "java.lang.Boolean" -> checkBox(parameter.displayName)
