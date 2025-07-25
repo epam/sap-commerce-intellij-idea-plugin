42653245f632f65f5d3f55b523db16c031b292bd
Mykhailo Lytvyn <lytvyn.michael@gmail.com>
2025-07-25 15:55:39 +0200

    - sr
    Signed-off-by: Mykhailo Lytvyn <lytvyn.michael@gmail.com>

--- src/com/intellij/idea/plugin/hybris/flexibleSearch/editor/FlexibleSearchInEditorResultsView.kt
+++ src/com/intellij/idea/plugin/hybris/flexibleSearch/editor/FlexibleSearchInEditorResultsView.kt
@@ -32,22 +32,10 @@ import com.intellij.openapi.fileTypes.PlainTextFileType
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
 import com.intellij.testFramework.LightVirtualFile
-import com.intellij.ui.ColoredTableCellRenderer
-import com.intellij.ui.JBColor
-import com.intellij.ui.SimpleTextAttributes
 import com.intellij.ui.dsl.builder.Align
 import com.intellij.ui.dsl.builder.panel
-import com.intellij.ui.render.RenderingUtil
-import com.intellij.ui.table.TableView
-import com.intellij.util.asSafely
-import com.intellij.util.ui.ColumnInfo
-import com.intellij.util.ui.JBUI
-import com.intellij.util.ui.ListTableModel
 import kotlinx.coroutines.CoroutineScope
-import java.awt.Dimension
-import java.io.Serial
 import javax.swing.JComponent
-import javax.swing.JTable
 
 @Service(Service.Level.PROJECT)
 class FlexibleSearchInEditorResultsView(
@@ -76,92 +64,14 @@ class FlexibleSearchInEditorResultsView(
             ?: multiResultsNotSupportedView()
     }
 
-    suspend fun resultsView(fileEditor: FlexibleSearchSplitEditor, content: String): JComponent {
-        return if (Plugin.GRID.isActive()) csvTableView(fileEditor, content)
-        else simpleTableView(fileEditor, content)
-    }
+    suspend fun resultsView(fileEditor: FlexibleSearchSplitEditor, content: String) = if (Plugin.GRID.isActive()) csvTableView(fileEditor, content)
+    else simpleTableView(content)
 
-    private fun simpleTableView(fileEditor: FlexibleSearchSplitEditor, content: String): JComponent {
-        val defaultCellRenderer = object : ColoredTableCellRenderer() {
-            @Serial
-            private val serialVersionUID: Long = -2610838431719623644L
-
-            override fun setToolTipText(text: String?) = Unit
-
-            override fun customizeCellRenderer(table: JTable, value: Any?, selected: Boolean, hasFocus: Boolean, row: Int, column: Int) {
-                val stringValue = value?.asSafely<String>() ?: return
-
-                if (column == 0) {
-                    append(stringValue, SimpleTextAttributes.GRAY_ATTRIBUTES)
-                    foreground = RenderingUtil.getForeground(table, selected)
-                    background = RenderingUtil.getBackground(table, selected)
-                    alignmentX = RIGHT_ALIGNMENT
-                } else {
-                    append(stringValue, SimpleTextAttributes.REGULAR_ATTRIBUTES)
-                    foreground = JBColor.lazy { JBUI.CurrentTheme.Table.foreground(selected, hasFocus) }
-                    background = JBColor(0xFFFFFF, 0x3C3F41)
-                }
-
-                border = JBUI.Borders.compound(
-                    JBUI.Borders.customLine(if (hasFocus) JBColor.blue else JBColor.border(), if (hasFocus && selected) 1 else 0, if (hasFocus && selected) 1 else 0, 1, 1),
-                    JBUI.Borders.empty(3)
-                )
-            }
-        }
-
-        val rows = content.trim().split("\n")
-        val headerRows = rows.first()
-            .split("|")
-            .toMutableList()
-            .apply { addFirst("") }
-        val headers = headerRows
-            .mapIndexed { index, columnName ->
-                object : ColumnInfo<List<String>, Any>(columnName) {
-                    override fun valueOf(item: List<String>?) = item?.getOrNull(index)
-                    override fun isCellEditable(item: List<String>?) = index != 0
-                    override fun getRenderer(item: List<String>?) = defaultCellRenderer
-                }
-            }
-            .toTypedArray<ColumnInfo<List<String>, Any>>()
-        val listTableModel = ListTableModel<List<String>>(*headers)
-
-        val dataRows = rows
-            .drop(1)
-            .mapIndexed { index, row ->
-                row.split("|")
-                    .map { it.trim() }
-                    .toMutableList()
-                    .apply { addFirst("${index + 1}") }
-            }
-
-        listTableModel.addRows(dataRows)
-        val tableView = TableView(listTableModel).apply {
-            autoResizeMode = JTable.AUTO_RESIZE_OFF
-            intercellSpacing = Dimension(0, 0)
-            autoResizeColumnsByHeader()
-        }
-        return panel {
-            row {
-                scrollCell(tableView)
-                    .align(Align.FILL)
-            }.resizableRow()
-        }
-    }
-
-    fun JTable.autoResizeColumnsByHeader() {
-        val header = tableHeader
-        val renderer = header.defaultRenderer
-
-        for (i in 0 until columnCount) {
-            val column = columnModel.getColumn(i)
-            val headerValue = column.headerValue
-            val component = renderer.getTableCellRendererComponent(this, headerValue, false, false, -1, i)
-            val preferredWidth = component.preferredSize.width + 32
-            column.preferredWidth = preferredWidth
-            column.minWidth = preferredWidth
-            column.maxWidth = preferredWidth
-            column.resizable = i > 0
-        }
+    private fun simpleTableView(content: String): JComponent = panel {
+        row {
+            scrollCell(FlexibleSearchSimplifiedTableView.of(content))
+                .align(Align.FILL)
+        }.resizableRow()
     }
 
     private suspend fun csvTableView(fileEditor: FlexibleSearchSplitEditor, content: String): JComponent {
