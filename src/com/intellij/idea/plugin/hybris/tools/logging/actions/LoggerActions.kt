5da57b7fe38d0938d98b5f29642ceb0905ad9233
Eugeni_Kalenchuk <eugeni_kalenchuk@epam.com>
2025-08-09 09:26:46 +0200

    SAP CX Logging | Show a spinner for Fetch button when loggers are being fetched from a remote server

--- src/com/intellij/idea/plugin/hybris/tools/logging/actions/LoggerActions.kt
+++ src/com/intellij/idea/plugin/hybris/tools/logging/actions/LoggerActions.kt
@@ -98,8 +98,7 @@ class FetchLoggersStateAction : AnAction() {
 
         e.presentation.text = if (state) "Refresh Loggers State" else "Fetch Loggers State"
         e.presentation.icon = if (state) HybrisIcons.Log.Action.REFRESH else HybrisIcons.Log.Action.FETCH
-        if (!loggerAccess.ready) {
-            e.presentation.disabledIcon = AnimatedIcon.Default.INSTANCE
-        }
+        e.presentation.disabledIcon = if (loggerAccess.ready) null else AnimatedIcon.Default.INSTANCE
+
     }
 }
