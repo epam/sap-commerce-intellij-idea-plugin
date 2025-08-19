ce5d435772381714ebb89395e084b35f25a27d65
Mykhailo Lytvyn <lytvyn.michael@gmail.com>
2025-08-19 14:58:19 +0200

    impex | moved live template

--- modules/impex/core/src/sap/commerce/toolset/impex/liveTemplates/ImpexContextType.kt
+++ modules/impex/core/src/sap/commerce/toolset/impex/liveTemplates/ImpexContextType.kt
@@ -1,7 +1,6 @@
 /*
- * This file is part of "SAP Commerce Developers Toolset" plugin for Intellij IDEA.
- * Copyright (C) 2014-2016 Alexander Bartash <AlexanderBartash@gmail.com>
- * Copyright (C) 2019-2023 EPAM Systems <hybrisideaplugin@epam.com> and contributors
+ * This file is part of "SAP Commerce Developers Toolset" plugin for IntelliJ IDEA.
+ * Copyright (C) 2019-2025 EPAM Systems <hybrisideaplugin@epam.com> and contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -16,6 +15,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 package sap.commerce.toolset.impex.liveTemplates
 
 import com.intellij.codeInsight.template.FileTypeBasedContextType
--- modules/impex/ui/src/sap/commerce/toolset/impex/ui/ImpExToAclEditorNotificationProvider.kt
+++ modules/impex/ui/src/sap/commerce/toolset/impex/ui/ImpExToAclEditorNotificationProvider.kt
@@ -15,10 +15,9 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 package sap.commerce.toolset.impex.ui
 
-import sap.commerce.toolset.impex.file.ImpexFileType
-import sap.commerce.toolset.impex.psi.ImpexUserRights
 import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.fileTypes.FileTypeRegistry
 import com.intellij.openapi.project.Project
@@ -30,6 +29,8 @@ import com.intellij.ui.EditorNotificationPanel
 import com.intellij.ui.EditorNotificationProvider
 import com.intellij.util.application
 import sap.commerce.toolset.HybrisIcons
+import sap.commerce.toolset.impex.file.ImpexFileType
+import sap.commerce.toolset.impex.psi.ImpexUserRights
 import sap.commerce.toolset.isHybrisProject
 import java.util.function.Function
 
