1462c7304127fb14529d25ac3838f9c4b038f636
Mykhailo Lytvyn <lytvyn.michael@gmail.com>
2025-08-19 14:53:39 +0200

    - repackaging

--- modules/impex/ui/src/sap/commerce/toolset/impex/actionSystem/ImpexFileCreateAction.kt
+++ modules/impex/ui/src/sap/commerce/toolset/impex/actionSystem/ImpexFileCreateAction.kt
@@ -1,6 +1,5 @@
 /*
  * This file is part of "SAP Commerce Developers Toolset" plugin for IntelliJ IDEA.
- * Copyright (C) 2014-2016 Alexander Bartash <AlexanderBartash@gmail.com>
  * Copyright (C) 2019-2025 EPAM Systems <hybrisideaplugin@epam.com> and contributors
  *
  * This program is free software: you can redistribute it and/or modify
@@ -16,6 +15,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 package sap.commerce.toolset.impex.actionSystem
 
 import com.intellij.ide.actions.CreateFileFromTemplateAction
