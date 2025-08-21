0022e4ddc40d17d57d06207a020814977722cee5
Eugeni_Kalenchuk <eugeni_kalenchuk@epam.com>
2025-08-21 16:09:06 +0200

    Create type renderer: Show an error message when an item type is not available

--- src/com/intellij/idea/plugin/hybris/debugger/ui/tree/render/DefaultModelRenderer.kt
+++ src/com/intellij/idea/plugin/hybris/debugger/ui/tree/render/DefaultModelRenderer.kt
@@ -1,6 +1,6 @@
 /*
  * This file is part of "SAP Commerce Developers Toolset" plugin for IntelliJ IDEA.
- * Copyright (C) 2019-2024 EPAM Systems <hybrisideaplugin@epam.com> and contributors
+ * Copyright (C) 2019-2025 EPAM Systems <hybrisideaplugin@epam.com> and contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -30,10 +30,13 @@ import com.intellij.debugger.ui.tree.render.ValueIconRenderer
 import com.intellij.ide.IdeBundle
 import com.intellij.idea.plugin.hybris.common.utils.HybrisI18NBundleUtils
 import com.intellij.idea.plugin.hybris.common.utils.HybrisIcons
+import com.intellij.idea.plugin.hybris.debugger.TypeRendererUtils
 import com.intellij.idea.plugin.hybris.notifications.Notifications
+import com.intellij.idea.plugin.hybris.system.type.meta.TSMetaModelAccess
 import com.intellij.notification.NotificationType
 import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
+import com.intellij.psi.search.GlobalSearchScope
 import com.sun.jdi.ObjectReference
 import com.sun.jdi.Type
 import com.sun.jdi.Value
@@ -63,6 +66,21 @@ class DefaultModelRenderer : CompoundRendererProvider() {
                     val value = valueDescriptor.value
                     val project = valueDescriptor.project
                     val className = value.type().name()
+                    val typeCode = TypeRendererUtils.toTypeCode(className)
+                    val metaAccess = TSMetaModelAccess.getInstance(project)
+                    val meta = metaAccess.findMetaItemByName(typeCode)
+
+                    if (meta == null) {
+                        TypeRendererUtils.notifyError("The item type $typeCode is not present in the *items.xml files.")
+                        return
+                    }
+
+                    val psiClass = DebuggerUtils.findClass(className, project, GlobalSearchScope.allScope(project))
+                    if (psiClass == null) {
+                        TypeRendererUtils.notifyError("The class for the item type $typeCode was not found. Rebuild the project and try again.")
+                        return
+                    }
+
                     val rendererName = ModelRenderer.createRendererName(className)
 
                     if (DumbService.isDumb(project)) {
