23f62dbf808d3045035d4a999816498070792147
Mykhailo Lytvyn <lytvyn.michael@gmail.com>
2025-08-22 23:15:59 +0200

    moved ProjectIntegrationsSettingsConfigurableProvider

--- modules/project/ui/src/sap/commerce/toolset/project/options/ProjectSettingsConfigurableProvider.kt
+++ modules/project/ui/src/sap/commerce/toolset/project/options/ProjectSettingsConfigurableProvider.kt
@@ -16,7 +16,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package sap.commerce.toolset.options
+package sap.commerce.toolset.project.options
 
 import com.intellij.openapi.options.BoundSearchableConfigurable
 import com.intellij.openapi.options.ConfigurableProvider
--- modules/shared/ui/src/sap/commerce/toolset/options/ApplicationSettingsConfigurableProvider.kt
+++ modules/shared/ui/src/sap/commerce/toolset/options/ApplicationSettingsConfigurableProvider.kt
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 package sap.commerce.toolset.options
 
 import com.intellij.openapi.options.BoundSearchableConfigurable
@@ -32,7 +33,7 @@ class ApplicationSettingsConfigurableProvider : ConfigurableProvider() {
         "[y] SAP Commerce", "[y] SAP CX configuration."
     ) {
 
-        private val applicationSettings = ApplicationSettings.getInstance()
+        private val applicationSettings = ApplicationSettings.Companion.getInstance()
 
         override fun createPanel() = panel {
             row {
