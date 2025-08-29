12588180c8f58de2d82a03d6680188f308062c9e
Eugeni_Kalenchuk <eugeni_kalenchuk@epam.com>
2025-08-29 14:57:57 +0200

    -cr

--- modules/logging/ui/src/sap/commerce/toolset/logging/ui/tree/nodes/LoggersHacConnectionNode.kt
+++ modules/logging/ui/src/sap/commerce/toolset/logging/ui/tree/nodes/LoggersHacConnectionNode.kt
@@ -53,24 +53,4 @@ class LoggersHacConnectionNode(
         }
         presentation.setIcon(connectionIcon)
     }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as LoggersHacConnectionNode
-
-        if (activeConnection != other.activeConnection) return false
-        if (connectionSettings != other.connectionSettings) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = activeConnection.hashCode()
-        result = 31 * result + connectionSettings.hashCode()
-        return result
-    }
-
-
 }
