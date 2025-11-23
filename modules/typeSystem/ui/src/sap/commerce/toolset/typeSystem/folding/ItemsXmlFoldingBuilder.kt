fd85a09d4823ea5ea70354b8aa8549dcb405ceb9
Mykhailo Lytvyn <lytvyn.michael@gmail.com>
2025-11-23 16:10:42 +0100

    #1666 | Type System | Improved folding for elements without required values

--- modules/typeSystem/ui/src/sap/commerce/toolset/typeSystem/folding/ItemsXmlFoldingBuilder.kt
+++ modules/typeSystem/ui/src/sap/commerce/toolset/typeSystem/folding/ItemsXmlFoldingBuilder.kt
@@ -80,11 +80,16 @@ class ItemsXmlFoldingBuilder : XmlFoldingBuilderEx<TSFoldingSettingsState, Items
 
             EnumTypes.ENUMTYPE -> psi.getAttributeValue(EnumType.CODE)
 
-            Persistence.COLUMNTYPE -> "[type] " + (psi.getAttributeValue(ColumnType.DATABASE)?.let { "$it : " } ?: "") + (psi.childrenOfType<XmlTag>()
-                .firstOrNull()
-                ?.value
-                ?.trimmedText
-                ?: "")
+            Persistence.COLUMNTYPE -> buildString {
+                append("[type] ")
+                psi.getAttributeValue(ColumnType.DATABASE)?.let { "$it : " }
+                    ?.let { append(it) }
+                psi.childrenOfType<XmlTag>()
+                    .firstOrNull()
+                    ?.value
+                    ?.trimmedText
+                    ?.let { append(it) }
+            }
 
             Relations.RELATION -> {
                 val code = psi.getAttributeValue(Relation.CODE) ?: "?"
@@ -99,56 +104,135 @@ class ItemsXmlFoldingBuilder : XmlFoldingBuilderEx<TSFoldingSettingsState, Items
                 "$code ($sourceType [$sourceRelation :: $targetRelation] $targetType)"
             }
 
-            Indexes.INDEX -> (psi.getAttributeValue(Index.NAME)
-                ?.let { tablify(psi, it, getCachedFoldingSettings(psi)?.tablifyItemIndexes, Indexes.INDEX, Index.NAME) }
-                ?: FALLBACK_PLACEHOLDER) + psi.childrenOfType<XmlTag>()
-                .mapNotNull { it.getAttributeValue(IndexKey.ATTRIBUTE) }
-                .joinToString(", ", TYPE_SEPARATOR)
+            Indexes.INDEX -> buildString {
+                val name = psi.getAttributeValue(Index.NAME)
+                    ?.let { tablify(psi, it, getCachedFoldingSettings(psi)?.tablifyItemIndexes, Indexes.INDEX, Index.NAME) }
+                    ?: FALLBACK_PLACEHOLDER
+                val indexes = psi.childrenOfType<XmlTag>()
+                    .mapNotNull { it.getAttributeValue(IndexKey.ATTRIBUTE) }
+                    .joinToString(", ", TYPE_SEPARATOR)
+                append(name)
+                append(indexes)
+            }
 
-            CustomProperties.PROPERTY -> (psi.getAttributeValue(CustomProperty.NAME)
-                ?.let { tablify(psi, it, getCachedFoldingSettings(psi)?.tablifyItemCustomProperties, CustomProperties.PROPERTY, CustomProperty.NAME) }
-                ?: FALLBACK_PLACEHOLDER) +
-                (psi.childrenOfType<XmlTag>()
+            CustomProperties.PROPERTY -> buildString {
+                val name = psi.getAttributeValue(CustomProperty.NAME)
+                    ?.let { tablify(psi, it, getCachedFoldingSettings(psi)?.tablifyItemCustomProperties, CustomProperties.PROPERTY, CustomProperty.NAME) }
+                    ?: FALLBACK_PLACEHOLDER
+                append(name)
+
+                psi.childrenOfType<XmlTag>()
                     .firstOrNull()
                     ?.value
                     ?.trimmedText
                     ?.let { TYPE_SEPARATOR + if (it.length > 50) it.substring(0, 50) + "..." else it }
-                    ?: "")
+                    ?.let { append(it) }
+            }
 
-            AtomicTypes.ATOMICTYPE -> psi.getAttributeValue(AtomicType.CLASS)
-                ?.let { tablify(psi, it, getCachedFoldingSettings(psi)?.tablifyAtomics, AtomicTypes.ATOMICTYPE, AtomicType.CLASS) } +
-                (psi.getAttributeValue(AtomicType.EXTENDS)?.let { TYPE_SEPARATOR + it } ?: "")
+            AtomicTypes.ATOMICTYPE -> buildString {
+                val clazz = psi.getAttributeValue(AtomicType.CLASS)
+                    ?.let { tablify(psi, it, getCachedFoldingSettings(psi)?.tablifyAtomics, AtomicTypes.ATOMICTYPE, AtomicType.CLASS) }
+                    ?.takeIf { it.isNotBlank() }
+                    ?: HybrisConstants.Folding.NO_VALUE
+                append(clazz)
 
-            MapTypes.MAPTYPE -> psi.getAttributeValue(MapType.CODE)
-                ?.let { tablify(psi, it, getCachedFoldingSettings(psi)?.tablifyMaps, MapTypes.MAPTYPE, MapType.CODE) } +
-                TYPE_SEPARATOR + psi.getAttributeValue(MapType.ARGUMENTTYPE)
-                ?.let { tablify(psi, it, getCachedFoldingSettings(psi)?.tablifyMaps, MapTypes.MAPTYPE, MapType.ARGUMENTTYPE, prepend = true) } +
-                " <-> " + psi.getAttributeValue(MapType.RETURNTYPE)
+                psi.getAttributeValue(AtomicType.EXTENDS)?.let { TYPE_SEPARATOR + it }
+                    ?.let { append(it) }
+            }
 
-            CollectionTypes.COLLECTIONTYPE -> psi.getAttributeValue(CollectionType.CODE)
-                ?.let { tablify(psi, it, getCachedFoldingSettings(psi)?.tablifyCollections, CollectionTypes.COLLECTIONTYPE, CollectionType.CODE) } +
-                TYPE_SEPARATOR +
-                psi.getAttributeValue(CollectionType.ELEMENTTYPE)
-                    ?.let { tablify(psi, it, getCachedFoldingSettings(psi)?.tablifyCollections, CollectionTypes.COLLECTIONTYPE, CollectionType.ELEMENTTYPE) } +
-                TYPE_SEPARATOR + (psi.getAttributeValue(CollectionType.TYPE)
-                ?: Type.COLLECTION.value)
+            MapTypes.MAPTYPE -> buildString {
+                val code = psi.getAttributeValue(MapType.CODE)
+                    ?.let { tablify(psi, it, getCachedFoldingSettings(psi)?.tablifyMaps, MapTypes.MAPTYPE, MapType.CODE) }
+                    ?.takeIf { it.isNotBlank() }
+                    ?: HybrisConstants.Folding.NO_VALUE
+                val argumentType = psi.getAttributeValue(MapType.ARGUMENTTYPE)
+                    ?.let { tablify(psi, it, getCachedFoldingSettings(psi)?.tablifyMaps, MapTypes.MAPTYPE, MapType.ARGUMENTTYPE, prepend = true) }
+                val returnType = psi.getAttributeValue(MapType.RETURNTYPE)
+
+                append(code)
+                append(TYPE_SEPARATOR)
+                append(argumentType)
+                append(" <-> ")
+                append(returnType)
+            }
+
+            CollectionTypes.COLLECTIONTYPE -> buildString {
+                val code = psi.getAttributeValue(CollectionType.CODE)
+                    ?.let { tablify(psi, it, getCachedFoldingSettings(psi)?.tablifyCollections, CollectionTypes.COLLECTIONTYPE, CollectionType.CODE) }
+                    ?.takeIf { it.isNotBlank() }
+                    ?: HybrisConstants.Folding.NO_VALUE
+                val elementType = psi.getAttributeValue(CollectionType.ELEMENTTYPE)
+                    ?.let { tablify(psi, it, getCachedFoldingSettings(psi)?.tablifyCollections, CollectionTypes.COLLECTIONTYPE, CollectionType.ELEMENTTYPE) }
+                    ?.takeIf { it.isNotBlank() }
+                    ?: HybrisConstants.Folding.NO_VALUE
+                val type = psi.getAttributeValue(CollectionType.TYPE)
+                    ?: Type.COLLECTION.value
+
+                append(code)
+                append(TYPE_SEPARATOR)
+                append(elementType)
+                append(TYPE_SEPARATOR)
+                append(type)
+            }
 
             Relation.SOURCE_ELEMENT,
-            Relation.TARGET_ELEMENT -> (psi.getAttributeValue(RelationElement.CARDINALITY) ?: Cardinality.MANY.value)
-                .let { if (it.length == Cardinality.ONE.value.length) "$it " else it } + TYPE_SEPARATOR +
-                tablifyRelationElement(psi) +
-                (psi.getAttributeValue(RelationElement.QUALIFIER)?.let { TYPE_SEPARATOR + it } ?: "")
+            Relation.TARGET_ELEMENT -> buildString {
+                val cardinality = psi.getAttributeValue(RelationElement.CARDINALITY) ?: Cardinality.MANY.value
+                append(cardinality.let { if (it.length == Cardinality.ONE.value.length) "$it " else it })
+                append(TYPE_SEPARATOR)
+                append(tablifyRelationElement(psi))
 
-            ItemTypes.ITEMTYPE -> psi.getAttributeValue(ItemType.CODE) +
-                TYPE_SEPARATOR + (psi.getAttributeValue(ItemType.EXTENDS)
-                ?: HybrisConstants.TS_TYPE_GENERIC_ITEM)
+                psi.getAttributeValue(RelationElement.QUALIFIER)?.let { TYPE_SEPARATOR + it }
+                    ?.let { append(it) }
+            }
 
-            ItemType.DEPLOYMENT -> "DB [" + psi.getAttributeValue(Deployment.TYPE_CODE) + TYPE_SEPARATOR + psi.getAttributeValue(Deployment.TABLE) + "]"
-            ItemType.DESCRIPTION -> "-- ${psi.value.trimmedText} --"
+            ItemTypes.ITEMTYPE -> buildString {
+                val code = psi.getAttributeValue(ItemType.CODE)
+                    ?.takeIf { it.isNotBlank() }
+                    ?: HybrisConstants.Folding.NO_VALUE
+                val extends = (psi.getAttributeValue(ItemType.EXTENDS)
+                    ?: HybrisConstants.TS_TYPE_GENERIC_ITEM)
 
-            Attributes.ATTRIBUTE -> psi.getAttributeValue(Attribute.QUALIFIER)
-                ?.let { tablify(psi, it, getCachedFoldingSettings(psi)?.tablifyItemAttributes, Attributes.ATTRIBUTE, Attribute.QUALIFIER) } +
-                TYPE_SEPARATOR + psi.getAttributeValue(Attribute.TYPE) + mandatory(psi)
+                append(code)
+                append(TYPE_SEPARATOR)
+                append(extends)
+            }
+
+            ItemType.DEPLOYMENT -> buildString {
+                val typeCode = psi.getAttributeValue(Deployment.TYPE_CODE)
+                    ?.takeIf { it.isNotBlank() }
+                    ?: HybrisConstants.Folding.NO_VALUE
+                val table = psi.getAttributeValue(Deployment.TABLE)
+                    ?.takeIf { it.isNotBlank() }
+                    ?: HybrisConstants.Folding.NO_VALUE
+
+                append("DB [")
+                append(typeCode)
+                append(TYPE_SEPARATOR)
+                append(table)
+                append("]")
+            }
+
+            ItemType.DESCRIPTION -> buildString {
+                append(HybrisConstants.Folding.DESCRIPTION_PREFIX)
+                append(' ')
+                append(psi.value.trimmedText)
+            }
+
+            Attributes.ATTRIBUTE -> buildString {
+                val qualifier = psi.getAttributeValue(Attribute.QUALIFIER)
+                    ?.let { tablify(psi, it, getCachedFoldingSettings(psi)?.tablifyItemAttributes, Attributes.ATTRIBUTE, Attribute.QUALIFIER) }
+                    ?.takeIf { it.isNotBlank() }
+                    ?: HybrisConstants.Folding.NO_VALUE
+                val type = psi.getAttributeValue(Attribute.TYPE)
+                    ?.takeIf { it.isNotBlank() }
+                    ?: HybrisConstants.Folding.NO_VALUE
+
+                append(qualifier)
+                append(TYPE_SEPARATOR)
+                append(type)
+                append(mandatory(psi))
+            }
 
             else -> FALLBACK_PLACEHOLDER
         }
@@ -195,8 +279,10 @@ class ItemsXmlFoldingBuilder : XmlFoldingBuilderEx<TSFoldingSettingsState, Items
 
         val longestLength = psi.parent.childrenOfType<XmlTag>()
             .filter { it.localName == Relation.TARGET_ELEMENT || it.localName == Relation.SOURCE_ELEMENT }
-            .mapNotNull { it.getAttributeValue(RelationElement.TYPE)
-                ?.let { relType -> relType + mandatory(it) }}
+            .mapNotNull {
+                it.getAttributeValue(RelationElement.TYPE)
+                    ?.let { relType -> relType + mandatory(it) }
+            }
             .maxOfOrNull { it.length }
             ?: value.length
         return value + " ".repeat(longestLength - value.length)
