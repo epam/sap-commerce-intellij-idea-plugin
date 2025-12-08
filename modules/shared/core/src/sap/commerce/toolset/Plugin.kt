5eb03341ddcd9508081e3b1628c11353e52a2f24
Mykhailo Lytvyn <lytvyn.michael@gmail.com>
2025-12-08 15:32:03 +0100

    #1688 | Other | Compatibility API changes for IDEA `2025.3` OpenSource Edition

--- modules/shared/core/src/sap/commerce/toolset/Plugin.kt
+++ modules/shared/core/src/sap/commerce/toolset/Plugin.kt
@@ -25,26 +25,27 @@ import com.intellij.openapi.project.Project
 import com.intellij.util.application
 import org.jetbrains.annotations.Nullable
 
-enum class Plugin(val id: String, val url: String? = null) {
+enum class Plugin(val id: String, val url: String? = null, val dependencies: Set<Plugin> = emptySet()) {
 
-    HYBRIS(HybrisConstants.PLUGIN_ID, "https://plugins.jetbrains.com/plugin/12867-sap-commerce-developers-toolset"),
-    JREBEL("JRebelPlugin", "https://plugins.jetbrains.com/plugin/4441-jrebel-and-xrebel"),
-    ANT_SUPPORT("AntSupport", "https://plugins.jetbrains.com/plugin/23025-ant"),
+    HYBRIS(HybrisConstants.PLUGIN_ID, url = "https://plugins.jetbrains.com/plugin/12867-sap-commerce-developers-toolset"),
+    ULTIMATE("com.intellij.modules.ultimate"),
+    JREBEL("JRebelPlugin", url = "https://plugins.jetbrains.com/plugin/4441-jrebel-and-xrebel"),
+    ANT_SUPPORT("AntSupport", url = "https://plugins.jetbrains.com/plugin/23025-ant"),
     MAVEN("org.jetbrains.idea.maven"),
-    KOTLIN("org.jetbrains.kotlin", "https://plugins.jetbrains.com/plugin/6954-kotlin"),
-    GROOVY("org.intellij.groovy", "https://plugins.jetbrains.com/plugin/1524-groovy"),
+    KOTLIN("org.jetbrains.kotlin", url = "https://plugins.jetbrains.com/plugin/6954-kotlin"),
+    GROOVY("org.intellij.groovy", url = "https://plugins.jetbrains.com/plugin/1524-groovy"),
     GRADLE("com.intellij.gradle"),
-    ANGULAR("AngularJS", "https://plugins.jetbrains.com/plugin/6971-angular"),
-    DATABASE("com.intellij.database", "https://plugins.jetbrains.com/plugin/10925-database-tools-and-sql-for-webstorm"),
+    ANGULAR("AngularJS", url = "https://plugins.jetbrains.com/plugin/6971-angular"),
+    DATABASE("com.intellij.database", url = "https://plugins.jetbrains.com/plugin/10925-database-tools-and-sql-for-webstorm", dependencies = setOf(ULTIMATE)),
     DIAGRAM("com.intellij.diagram"),
-    PROPERTIES("com.intellij.properties", "https://plugins.jetbrains.com/plugin/11594-properties"),
-    COPYRIGHT("com.intellij.copyright", "https://plugins.jetbrains.com/plugin/13114-copyright"),
-    JAVASCRIPT("JavaScript", "https://plugins.jetbrains.com/plugin/22069-javascript-and-typescript"),
-    JAVAEE("com.intellij.javaee", "https://plugins.jetbrains.com/plugin/20207-jakarta-ee-platform"),
-    JAVAEE_WEB("com.intellij.javaee.web", "https://plugins.jetbrains.com/plugin/20216-jakarta-ee-web-servlets"),
-    JAVAEE_EL("com.intellij.javaee.el", "https://plugins.jetbrains.com/plugin/20208-jakarta-ee-expression-language-el-"),
-    SPRING("com.intellij.spring", "https://plugins.jetbrains.com/plugin/20221-spring"),
-    CRON("com.intellij.cron", "https://plugins.jetbrains.com/plugin/24438-cron-expressions"),
+    PROPERTIES("com.intellij.properties", url = "https://plugins.jetbrains.com/plugin/11594-properties"),
+    COPYRIGHT("com.intellij.copyright", url = "https://plugins.jetbrains.com/plugin/13114-copyright"),
+    JAVASCRIPT("JavaScript", url = "https://plugins.jetbrains.com/plugin/22069-javascript-and-typescript", dependencies = setOf(ULTIMATE)),
+    JAVAEE("com.intellij.javaee", url = "https://plugins.jetbrains.com/plugin/20207-jakarta-ee-platform", dependencies = setOf(ULTIMATE)),
+    JAVAEE_WEB("com.intellij.javaee.web", url = "https://plugins.jetbrains.com/plugin/20216-jakarta-ee-web-servlets", dependencies = setOf(ULTIMATE)),
+    JAVAEE_EL("com.intellij.javaee.el", url = "https://plugins.jetbrains.com/plugin/20208-jakarta-ee-expression-language-el-", dependencies = setOf(ULTIMATE)),
+    SPRING("com.intellij.spring", url = "https://plugins.jetbrains.com/plugin/20221-spring", dependencies = setOf(ULTIMATE)),
+    CRON("com.intellij.cron", url = "https://plugins.jetbrains.com/plugin/24438-cron-expressions", dependencies = setOf(ULTIMATE)),
     GRID("intellij.grid.plugin"),
     JAVA_I18N("com.intellij.java-i18n");
 
@@ -55,7 +56,9 @@ enum class Plugin(val id: String, val url: String? = null) {
         @Nullable
         get() = PluginManagerCore.getPlugin(pluginId)
 
-    fun isActive() = PluginManagerCore.isLoaded(pluginId) && !PluginManagerCore.isDisabled(pluginId)
+    fun isActive() = isActive(pluginId) && dependencies.all { isActive(it.pluginId) }
+
+    private fun isActive(pluginId: PluginId): Boolean = PluginManagerCore.isLoaded(pluginId) && !PluginManagerCore.isDisabled(pluginId)
 
     fun <T> ifActive(operation: () -> T): T? = if (isActive()) operation() else null
 
@@ -66,4 +69,7 @@ enum class Plugin(val id: String, val url: String? = null) {
     fun <T> service(clazz: Class<T>): T? = ifActive { application.getService(clazz) }
     fun <T> service(project: Project, clazz: Class<T>): T? = ifActive { project.getService(clazz) }
 
+    companion object {
+        fun of(id: String) = Plugin.entries.find { it.id == id }
+    }
 }
