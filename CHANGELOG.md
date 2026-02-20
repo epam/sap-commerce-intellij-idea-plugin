acd83a33ab24ba1ba5a5f961423cedf82240c1c9
Mykhailo Lytvyn <lytvyn.michael@gmail.com>
2026-02-20 22:01:44 +0100

    CCv2 | Hide `delete a build` action for deployed builds

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,10 +2,13 @@
 
 <cite>Release contributors</cite>
 
-- 1 PR(s) by [Mykhailo Lytvyn](https://github.com/epam/sap-commerce-intellij-idea-plugin/pulls?q=milestone%3A2026.0.0+author%3Amlytvyn+is%3Apr)
+- 2 PR(s) by [Mykhailo Lytvyn](https://github.com/epam/sap-commerce-intellij-idea-plugin/pulls?q=milestone%3A2026.0.0+author%3Amlytvyn+is%3Apr)
 
 ### Other
 - Compatibility release for IDE 2026 [#1741](https://github.com/epam/sap-commerce-intellij-idea-plugin/pull/1741)
+
+### `CCv2` enhancements
+- Hide `delete a build` action for deployed builds [#1742](https://github.com/epam/sap-commerce-intellij-idea-plugin/pull/1742)
 
 ## [2025.3.3.2]
 
--- modules/ccv2/ui/src/sap/commerce/toolset/ccv2/ui/view/CCv2BuildsDataView.kt
+++ modules/ccv2/ui/src/sap/commerce/toolset/ccv2/ui/view/CCv2BuildsDataView.kt
@@ -1,6 +1,6 @@
 /*
  * This file is part of "SAP Commerce Developers Toolset" plugin for IntelliJ IDEA.
- * Copyright (C) 2019-2025 EPAM Systems <hybrisideaplugin@epam.com> and contributors
+ * Copyright (C) 2019-2026 EPAM Systems <hybrisideaplugin@epam.com> and contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -117,7 +117,7 @@ object CCv2BuildsDataView : CCv2DataView<CCv2BuildDto>() {
 
             panel {
                 row {
-                    icon(build.status.icon)
+                    icon(build.statusIcon)
                         .gap(RightGap.SMALL)
                     label(build.status.title)
                         .comment("Status")
