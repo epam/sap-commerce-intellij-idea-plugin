4c41d8077056ed9f7515eddd7f6e21baf57917c6
Mykhailo Lytvyn <lytvyn.michael@gmail.com>
2026-03-24 22:57:57 +0100

    #1778 | ImpEx | Local Fix: improved `Inject Config import processor` fix

--- modules/impex/core/src/sap/commerce/toolset/impex/codeInspection/ImpExConfigProcessorInspection.kt
+++ modules/impex/core/src/sap/commerce/toolset/impex/codeInspection/ImpExConfigProcessorInspection.kt
@@ -1,6 +1,6 @@
 /*
  * This file is part of "SAP Commerce Developers Toolset" plugin for IntelliJ IDEA.
- * Copyright (C) 2019-2025 EPAM Systems <hybrisideaplugin@epam.com> and contributors
+ * Copyright (C) 2019-2026 EPAM Systems <hybrisideaplugin@epam.com> and contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -94,10 +94,9 @@ class ImpExConfigProcessorInspection : LocalInspectionTool() {
                 else PsiTreeUtil.skipSiblingsForward(firstChild, PsiComment::class.java, LeafPsiElement::class.java)
 
                 insertBefore
-                    ?.let { it.addBefore(config, it) }
+                    ?.let { file.addBefore(config, it) }
             }
         }
-
     }
 }
 
