c41e6a44759deae5030a33861f2509c56387e3b9
Mykhailo Lytvyn <lytvyn.michael@gmail.com>
2026-03-27 14:39:28 +0100

    ImpEx | Do not create TS reference when `&DocId` reference present

--- modules/impex/core/src/sap/commerce/toolset/impex/lang/findUsages/ImpExFindUsagesProvider.kt
+++ modules/impex/core/src/sap/commerce/toolset/impex/lang/findUsages/ImpExFindUsagesProvider.kt
@@ -1,7 +1,7 @@
 /*
  * This file is part of "SAP Commerce Developers Toolset" plugin for IntelliJ IDEA.
  * Copyright (C) 2014-2016 Alexander Bartash <AlexanderBartash@gmail.com>
- * Copyright (C) 2019-2025 EPAM Systems <hybrisideaplugin@epam.com> and contributors
+ * Copyright (C) 2019-2026 EPAM Systems <hybrisideaplugin@epam.com> and contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -36,6 +36,9 @@ class ImpExFindUsagesProvider : FindUsagesProvider {
             TokenSet.create(ImpExTypes.MACRO_NAME_DECLARATION),
             TokenSet.create(ImpExTypes.MACRO_DECLARATION),
             TokenSet.create(ImpExTypes.MACRO_USAGE),
+            TokenSet.create(ImpExTypes.DOCUMENT_ID),
+            TokenSet.create(ImpExTypes.DOCUMENT_ID_USAGE),
+            TokenSet.create(ImpExTypes.DOCUMENT_ID_DEC),
             TokenSet.create(ImpExTypes.FIELD_VALUE),
         ),
         TokenSet.create(
