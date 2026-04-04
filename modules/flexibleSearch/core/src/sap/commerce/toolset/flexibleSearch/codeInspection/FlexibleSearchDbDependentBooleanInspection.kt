8995263f38ae7e2ddfd1b32e81995e096338edd6
Mykhailo Lytvyn <lytvyn.michael@gmail.com>
2026-04-04 19:27:23 +0200

    FlexibleSearch | Inspection: db dependent boolean literal should be ommited

---
+++ modules/flexibleSearch/core/src/sap/commerce/toolset/flexibleSearch/codeInspection/FlexibleSearchDbDependentBooleanInspection.kt
@@ -0,0 +1,50 @@
+/*
+ * This file is part of "SAP Commerce Developers Toolset" plugin for IntelliJ IDEA.
+ * Copyright (C) 2019-2026 EPAM Systems <hybrisideaplugin@epam.com> and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package sap.commerce.toolset.flexibleSearch.codeInspection
+
+import com.intellij.codeHighlighting.HighlightDisplayLevel
+import com.intellij.codeInspection.LocalInspectionTool
+import com.intellij.codeInspection.ProblemHighlightType
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiElementVisitor
+import sap.commerce.toolset.flexibleSearch.codeInspection.fix.FlexibleSearchChangeBooleanQuickFix
+import sap.commerce.toolset.flexibleSearch.psi.FlexibleSearchLiteralExpression
+import sap.commerce.toolset.flexibleSearch.psi.FlexibleSearchVisitor
+
+class FlexibleSearchDbDependentBooleanInspection : LocalInspectionTool() {
+
+    override fun getDefaultLevel(): HighlightDisplayLevel = HighlightDisplayLevel.WARNING
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor = FlexibleSearchPsiVisitor(holder)
+
+    private class FlexibleSearchPsiVisitor(private val problemsHolder: ProblemsHolder) : FlexibleSearchVisitor() {
+
+        override fun visitLiteralExpression(element: FlexibleSearchLiteralExpression) {
+            element.booleanLiteral ?: return
+
+            val oldValue = element.text.trim()
+            val newValue = if (oldValue.equals("true", true)) "1" else "0"
+
+            problemsHolder.registerProblem(
+                element,
+                "Replace '$oldValue' boolean expression with '$newValue",
+                ProblemHighlightType.WARNING,
+                FlexibleSearchChangeBooleanQuickFix(element, oldValue, newValue)
+            )
+        }
+    }
+}
