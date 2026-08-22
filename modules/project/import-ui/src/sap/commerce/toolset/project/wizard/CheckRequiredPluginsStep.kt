6f9487b761fba8ff4a9b00ac80c8fd77476c6e4c
Mykhailo Lytvyn <lytvyn.michael@gmail.com>
2026-08-22 11:27:32 +0200

    Project Import | Skip state validation of the built-in plugins during the import

--- modules/project/import-ui/src/sap/commerce/toolset/project/wizard/CheckRequiredPluginsStep.kt
+++ modules/project/import-ui/src/sap/commerce/toolset/project/wizard/CheckRequiredPluginsStep.kt
@@ -130,6 +130,7 @@ class CheckRequiredPluginsStep(context: WizardContext) : ProjectImportWizardStep
             .asSequence()
             .filterIsInstance<PluginDetailsService.ModuleDependencyInfo.OnPlugin>()
             .distinctBy { it.pluginId }
+            .filterNot { pluginDetailsService.isBuiltIn(it.pluginId) }
             .onEach { pluginDependency ->
                 val pluginId = pluginDependency.pluginId
                 if (!PluginManager.isPluginInstalled(pluginId)) {
@@ -138,7 +139,7 @@ class CheckRequiredPluginsStep(context: WizardContext) : ProjectImportWizardStep
                 }
 
                 val notEnabled = Plugin.of(pluginId.idString)?.isDisabled()
-                    ?: (!pluginDetailsService.isLoaded(pluginId) || pluginDetailsService.isDisabled(pluginId))
+                    ?: (!pluginDetailsService.isLoaded(pluginId) || pluginDetailsService.isBuiltIn(pluginId))
 
                 if (notEnabled) notEnabledModel.add(pluginId)
             }
